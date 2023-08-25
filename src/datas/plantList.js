77c1286969941e5211f497abd82bcdd89edfb4ae
Steve <stevele99@yahoo.fr>
2023-08-25 10:45:06 +0200

    description maj

--- src/datas/plantList.js
+++ src/datas/plantList.js
@@ -16,7 +16,8 @@ export const plantList = [
 		light: 1,
 		water: 3,
 		cover: monstera,
-		price: 15
+		price: 15,
+		description:"Ce sont des lianes sempervirentes, pouvant grimper dans les arbres jusqu'à 20 m de hauteur. Ces lianes produisent des racines aériennes qui renforcent l'accrochage de la plante sur son support et qui peuvent aussi rejoindre le sol pour s'y replanter."
 	},
 	{
 		name: 'ficus lyrata',
@@ -26,7 +27,8 @@ export const plantList = [
 		light: 3,
 		water: 1,
 		cover: lyrata,
-		price: 16
+		price: 16,
+		description:"Ficus lyrata est une espèce de figuier de la famille des Moraceae. Elle est originaire d'Afrique de l'Ouest. Son habitat va de l'ouest du Cameroun à la Sierra Leone, dans la forêt tropicale pluvieuse. Il s'agit d'un arbre ornemental commun dans les jardins tropicaux et subtropicaux, parfois utilisée comme plante verte d'intérieur."
 
 	},
 
@@ -37,8 +39,8 @@ export const plantList = [
 		light: 1,
 		water: 2,
 		cover: pothos,
-
-		price: 9
+		price: 9,
+		description:"Le Pothos argenté est une plante tropicale grimpante originaire d'Asie. Faisant partie de la famille des Araceae, il est caractérisé par son feuillages en forme d'As et ses taches d'argent. Pouvant supporter des températures allant jusqu'à 15°C, le pothos argenté est un bon compagnon d'intérieur."
 	},
 	{
 		name: 'calathea',
@@ -47,8 +49,8 @@ export const plantList = [
 		light: 2,
 		water: 3,
 		cover: calathea,
-
-		price: 20
+		price: 20,
+		description:"Le Calathea zebrina est appelé la plante zèbre. Son feuillage, régulièrement strié de bandes vert sombre sur un fond vert clair, est épais et disposé à l'horizontale."
 	},
 	{
 		name: 'olivier',
@@ -57,7 +59,8 @@ export const plantList = [
 		light: 2,
 		water: 1,
 		cover: olivier,
-		price: 25
+		price: 25,
+		description:"Arbre appartenant à la famille des Oléacées, au tronc noueux, à petites feuilles persistantes de forme ovale-lancéolée d'un vert foncé au-dessus, argenté en dessous, dont le fruit est une drupe comestible et oléagineuse (v. olive)."
 	},
 
 	{
@@ -67,7 +70,8 @@ export const plantList = [
 		light: 2,
 		water: 3,
 		cover: cactus,
-		price: 6
+		price: 6,
+		description:"Les cactus sont des plantes grasses, dont la caractéristique première est de posséder des aréoles, sortes de creux dans leur épiderme d'où partent épines, fleurs, feuilles (rarement), nouveaux segments ou glochides (qui sont des épines toutes petites et regroupées)."
 	},
 	{
 		name: 'basilique',
@@ -76,7 +80,8 @@ export const plantList = [
 		light: 2,
 		water: 3,
 		cover: basil,
-		price: 5
+		price: 5,
+		description:"Le basilic, ou Ocimum basilicum, connu aussi sous les noms d'oranger des savetiers ou herbe aux sauces, est une plante condimentaire annuelle originaire d'Inde et appartenant à la famille des Lamiacées. C'est une herbacée de 20 à 40 cm, qui peut atteindre 80 cm."
 	},
 	{
 		name: 'succulente',
@@ -85,7 +90,8 @@ export const plantList = [
 		light: 2,
 		water: 1,
 		cover: succulent,
-		price: 8
+		price: 8,
+		description:"“Succulentes” provient du mot latin “suculentus”, qui signifie « plein de suc ». Ces plantes sont en effet capables de stocker de l'eau dans leurs feuilles, leurs tiges et leurs racines, et de la transformer en suc mucilagineux qui leur permet de survivre dans les milieux les plus hostiles."
 	},
 
 	{
@@ -95,6 +101,7 @@ export const plantList = [
 		light: 2,
 		water: 2,
 		cover: mint,
-		price: 4
+		price: 4,
+		description:"La menthe est une plante pleine d'ardeur, d'une couleur éclatante, vert vif à vert gris argenté, qui propose, selon les variétés, une gamme étendue de parfums, d'arômes et de saveurs intenses et rafraîchissantes."
 	}
 ]
